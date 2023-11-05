b6482de94d1192cd7419f24c6cb9d1d44d9f91d9
mehmetakifkucukkaya <mehmet.akif.20021@gmail.com>
2023-11-05 21:30:56 +0300

    Create Footer and finish project

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import Analytcis from "./components/Analytics"
 import Cards from "./components/Cards"
+import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Newsletter from "./components/Newsletter"
 
 
 function App() {
- 
+
 
   return (
     <>
-     <Navbar/>
-     <Hero/>
-     <Analytcis/>
-     <Newsletter/>
-     <Cards/>
-     
+      <Navbar />
+      <Hero />
+      <Analytcis />
+      <Newsletter />
+      <Cards />
+      <Footer />
     </>
   )
 }
